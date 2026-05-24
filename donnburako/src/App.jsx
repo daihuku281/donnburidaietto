381208a58b60d4ad9da311300be454a41ee05495
kotare1112 <kotare1112@gmail.com>
2026-05-24 10:19:35 +0900

    デデデを巨大化

--- donnburako/src/App.jsx
+++ donnburako/src/App.jsx
@@ -172,7 +172,7 @@ function App() {
             <img
                 src={encodeURI('/images/スクリーンショット 2026-05-23 20.38.20.png')}
                 alt="decoration"
-                style={{ position: 'fixed', bottom: '4rem', right: '-8rem', width: '700px' }}
+                style={{ position: 'fixed', bottom: '4rem', right: '-8rem', width: '800px' }}
             />
         </div>
     )
